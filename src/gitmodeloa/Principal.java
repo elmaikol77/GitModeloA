9ef823e71f37f0244402c1d88958686de9c12361
elmaikol77 <134549714+elmaikol77@users.noreply.github.com>
2025-06-20 12:26:54 +0200

    Update Principal.java desde Remoto

--- src/gitmodeloa/Principal.java
+++ src/gitmodeloa/Principal.java
@@ -7,6 +7,8 @@ public class Principal {
 		System.out.println("Hola a todos");
 		System.out.println("Cambio en master para merge");
 		System.out.println("Cambio Rama 1");
+		System.out.println("Cambio desde Repositorio Remoto");
+		
 
 	}
 
